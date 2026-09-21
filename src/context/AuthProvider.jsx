5dd195c961391bc48d3072d2c9d1615fb1416b16
Big-Smokkee <fardinfaraz13579@gmail.com>
2026-09-21 21:46:47 +0600

    feat: add sign up user with email and password

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,18 +1,23 @@
 import { useState } from 'react';
 import { AuthContext } from './AuthContext';
+import { auth } from '../firebase/config'
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 const AuthProvider = ({ children }) => {
     // here I will write every states
     const [user, setUser] = useState(null);
     // here I will write each and every authentication related functions
-
+    const signUpUserWithEmailAndPassword = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
 
     //observer set
 
     // This is the object which would be passed into the AuthContext 
     const authInfo = {
         user,
-        setUser
+        setUser,
+        signUpUserWithEmailAndPassword
     }
     return (
         <AuthContext value={authInfo}>
